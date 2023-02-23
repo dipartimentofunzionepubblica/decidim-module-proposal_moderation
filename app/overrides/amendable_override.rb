d8c31fcdfee52071d64f947bb57f85f295f28c0b
Lorenzo Angelone <l.angelone@kapusons.it>
2023-02-23 13:59:02 +0100

    fixed visible_emendations_for without current_user

--- app/overrides/amendable_override.rb
+++ app/overrides/amendable_override.rb
@@ -11,8 +11,13 @@ module Decidim
     def visible_emendations_for(user)
       published_emendations = emendations.published
       if component.current_settings.moderation_amendment_enabled
-        published_emendations = emendations.not_in_moderation_rejected.not_in_review.or(emendations.in_moderation_rejected_for_user(user).or(emendations.in_review_for_user(user)))
-        published_emendations = published_emendations.where.not(state: 'review_accepted') unless emendations.published.map(&:authors).flatten.include?(user)
+        if user
+          published_emendations = emendations.not_in_moderation_rejected.not_in_review.or(emendations.in_moderation_rejected_for_user(user).or(emendations.in_review_for_user(user)))
+          published_emendations = published_emendations.where.not(state: 'review_accepted') unless emendations.published.map(&:authors).flatten.include?(user)
+        else
+          published_emendations = emendations.not_in_moderation_rejected.not_in_review
+          published_emendations = published_emendations.where.not(state: 'review_accepted') unless emendations.published.map(&:authors).flatten.include?(user)
+        end
       end
       return published_emendations unless component.settings.amendments_enabled
 
